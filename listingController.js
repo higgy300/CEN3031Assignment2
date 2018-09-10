61006352ca860c35aefcda4cd323b6a9bfe87810
higgy300 <jhigueraballesteros@gmail.com>
2018-09-10 14:22:01 -0400

    updating index.html and listingController.js

--- listingController.js
+++ listingController.js
@@ -1,14 +1,42 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    $scope.show = false;
+    $scope.form ={};
+
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push($scope.form);
+      $scope.form = {};
+      $scope.show = !$scope.show;
+    };
+
+
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+      /*let newListings = [];
+        newListings = $scope.listings.filter(function (index) {
+          let name = listing.name;
+          let code = listing.code;
+
+        });
+        $scope.listings = newListings;
+        $scope.detailedInfo = undefined;*/
+    };
+
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = index;
+    };
+
+
+    $scope.clickListener = function() {
+      $scope.show = !$scope.show;
+    };
   }
 ]);
